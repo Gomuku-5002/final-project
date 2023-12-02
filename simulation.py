26bfcb7fb685ca970861f97599dbb3d1cbf34e96
Erica Gao <61401388+EricaGao@users.noreply.github.com>
2023-12-02 17:20:55 +0800

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 16:15:08 2023
+
+@author: erica
+"""
+import numpy as np
+shape_score = [(50, (0, 1, 1, 0, 0)),
+               (50, (0, 0, 1, 1, 0)),
+               (200, (1, 1, 0, 1, 0)),
+               (500, (0, 0, 1, 1, 1)),
+               (500, (1, 1, 1, 0, 0)),
+               (5000, (0, 1, 1, 1, 0)),
+               (5000, (0, 1, 0, 1, 1, 0)),
+               (5000, (0, 1, 1, 0, 1, 0)),
+               (5000, (1, 1, 1, 0, 1)),
+               (5000, (1, 1, 0, 1, 1)),
+               (5000, (1, 0, 1, 1, 1)),
+               (5000, (1, 1, 1, 1, 0)),
+               (5000, (0, 1, 1, 1, 1)),
+               (50000, (0, 1, 1, 1, 1, 0)),
+               (99999999, (1, 1, 1, 1, 1))]
+
+ratio = 1
+# list1 = [(6, 7), (6, 8), (6, 9), (6, 10)]
+# list2 = [(6, 5), (6, 6)]
+def evaluation(is_ai, board):
+    # Create the extended chess board.
+    xbline=19
+    bline = 11
+    ext_board = np.zeros((xbline,xbline), dtype=int)
+    ext_board[4:4+bline,4:4+bline]=board.copy()
+    ext_board[0:4,:] = ext_board[bline:4+bline,:]
+    ext_board[4+bline:8+bline,:] = ext_board[4:8,:] 
+    ext_board[:,0:4] = ext_board[:,bline:4+bline]
+    ext_board[:,4+bline:8+bline] = ext_board[:,4:8]
+    # List1 is black stone; list2 is white.
+    list1 = (np.argwhere(board==1)+4).tolist()
+    list2 = (np.argwhere(board==-1)+4).tolist()
+    total_score = 0
+
+    if is_ai:
+        my_list = list1
+        enemy_list = list2
+    else:
+        my_list = list2
+        enemy_list = list1
+
+    # 算自己的得分
+    score_all_arr = []  # 得分形状的位置 用于计算如果有相交 得分翻倍
+    my_score = 0
+    for pt in my_list:
+        m = pt[0]
+        n = pt[1]
+        my_score += cal_score(m, n, 0, 1, enemy_list, my_list, score_all_arr)
+        my_score += cal_score(m, n, 1, 0, enemy_list, my_list, score_all_arr)
+        my_score += cal_score(m, n, 1, 1, enemy_list, my_list, score_all_arr)
+        my_score += cal_score(m, n, -1, 1, enemy_list, my_list, score_all_arr)
+
+    #  算敌人的得分， 并减去
+    score_all_arr_enemy = []
+    enemy_score = 0
+    for pt in enemy_list:
+        m = pt[0]
+        n = pt[1]
+        enemy_score += cal_score(m, n, 0, 1, my_list, enemy_list, score_all_arr_enemy)
+        enemy_score += cal_score(m, n, 1, 0, my_list, enemy_list, score_all_arr_enemy)
+        enemy_score += cal_score(m, n, 1, 1, my_list, enemy_list, score_all_arr_enemy)
+        enemy_score += cal_score(m, n, -1, 1, my_list, enemy_list, score_all_arr_enemy)
+
+    total_score = my_score - enemy_score*ratio*0.1
+
+    return total_score
+
+def cal_score(m, n, x_decrict, y_derice, enemy_list, my_list, score_all_arr):
+    add_score = 0  # 加分项
+    # 在一个方向上， 只取最大的得分项
+    max_score_shape = (0, None)
+
+    # 如果此方向上，该点已经有得分形状，不重复计算
+    for item in score_all_arr:
+        for pt in item[1]:
+            if m == pt[0] and n == pt[1] and x_decrict == item[2][0] and y_derice == item[2][1]:
+                return 0
+
+    # 在落子点 左右方向上循环查找得分形状
+    for offset in range(-5, 1):
+        # offset = -2
+        pos = []
+        for i in range(0, 6):
+            if [m + (i + offset) * x_decrict, n + (i + offset) * y_derice] in enemy_list:
+                pos.append(2)
+            elif [m + (i + offset) * x_decrict, n + (i + offset) * y_derice] in my_list:
+                pos.append(1)
+            else:
+                pos.append(0)
+        tmp_shap5 = (pos[0], pos[1], pos[2], pos[3], pos[4])
+        tmp_shap6 = (pos[0], pos[1], pos[2], pos[3], pos[4], pos[5])
+
+        for (score, shape) in shape_score:
+            if tmp_shap5 == shape or tmp_shap6 == shape:
+                if tmp_shap5 == (1,1,1,1,1):
+                    print('wwwwwwwwwwwwwwwwwwwwwwwwwww')
+                if score > max_score_shape[0]:
+                    max_score_shape = (score, ((m + (0+offset) * x_decrict, n + (0+offset) * y_derice),
+                                               (m + (1+offset) * x_decrict, n + (1+offset) * y_derice),
+                                               (m + (2+offset) * x_decrict, n + (2+offset) * y_derice),
+                                               (m + (3+offset) * x_decrict, n + (3+offset) * y_derice),
+                                               (m + (4+offset) * x_decrict, n + (4+offset) * y_derice)), (x_decrict, y_derice))
+
+    # 计算两个形状相交， 如两个3活 相交， 得分增加 一个子的除外
+    if max_score_shape[1] is not None:
+        for item in score_all_arr:
+            for pt1 in item[1]:
+                for pt2 in max_score_shape[1]:
+                    if pt1 == pt2 and max_score_shape[0] > 10 and item[0] > 10:
+                        add_score += item[0] + max_score_shape[0]
+
+        score_all_arr.append(max_score_shape)
+
+    return add_score + max_score_shape[0]
+
+test = np.zeros((11,11))
+test[6,7]=1
+test[6,8]=1
+test[6,9]=1
+test[7,7]=-1
+# test[7,8]=-1
+print(evaluation(True, test))
